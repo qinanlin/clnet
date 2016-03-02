f88ad7072ad357cb76ec8f17e8ec399c052fb084
CL <qinanlin12@foxmail.com>
2016-03-02 21:14:00 +0800

    del .a .d .o

--- logapi/logapi.d
+++
@@ -1,6 +0,0 @@
-logapi.o: logapi.cpp logapi.h /mnt/sharefile/src/shmchannel/shmchannel.h \
- /mnt/sharefile/src/common/CommMisc.h \
- /mnt/sharefile/src/sharemem/sharemem.h \
- /mnt/sharefile/src/sharemem/sharemem.cpp \
- /mnt/sharefile/src/sharemem/sharemem.h \
- /mnt/sharefile/src/logsvr/logsys.h
